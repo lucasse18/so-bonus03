6e1ca826bbc2869434b31a8e311e3b9a726c7604
Lucas Santos Eleuterio <lucasse16@gmail.com>
2016-10-04 15:30:22 -0300

    tabs to spaces

--- src/produtor-sema.c
+++ src/produtor-sema.c
@@ -10,7 +10,7 @@
 #define BUFF_SZ (MEM_SZ - sizeof(size_t))
 
 typedef struct shared_area {
-	char buffer[BUFF_SZ];
+  char buffer[BUFF_SZ];
 } sa_t;
 
 unsigned long bytes = 0;
@@ -24,23 +24,23 @@ int main() {
   signal(SIGTERM, term_handler);
 
   key_t key = 1234;
-	int shmid = shmget(key, MEM_SZ, 0644|IPC_CREAT);
-	void *shared_memory = shmat(shmid, (void*)0, 0);
+  int shmid = shmget(key, MEM_SZ, 0644|IPC_CREAT);
+  void *shared_memory = shmat(shmid, (void*)0, 0);
 
   printf("ID da memoria compartilhada   = %d\n", shmid);
-	printf("ADDR da memoria compartilhada = %p\n", shared_memory);
+  printf("ADDR da memoria compartilhada = %p\n", shared_memory);
 
-	sa_t *sa_ptr = (sa_t *) shared_memory;
+  sa_t *sa_ptr = (sa_t *) shared_memory;
   sem_t *ocupado = sem_open("pro_con_sem_ocupado", O_CREAT, 0644, 0);
   sem_t *livre   = sem_open("pro_con_sem_livre", O_CREAT, 0644, BUFF_SZ);
 
   size_t i = 0;
-	for(;;) {
+  for(;;) {
     sem_wait(livre);
     sa_ptr->buffer[i] = '#';
     sem_post(ocupado);
     i = (i+1)%BUFF_SZ;
     bytes++;
-	}
+  }
   return 0;
 }
--- src/produtor-slock.c
+++ src/produtor-slock.c
@@ -10,43 +10,43 @@
 #define BUFF_SZ (MEM_SZ - sizeof(size_t) - (2 * sizeof(sem_t)))
 
 typedef struct shared_area{
-	size_t num;
-	char buffer[BUFF_SZ];
+  size_t num;
+  char buffer[BUFF_SZ];
 } sa_t;
 
 int main() {
   size_t i;
-	key_t key = 1234;
-	sa_t *sa_ptr;
+  key_t key = 1234;
+  sa_t *sa_ptr;
 
-	int shmid = shmget(key, MEM_SZ, 0666|IPC_CREAT);
-	if (shmid == -1) {
+  int shmid = shmget(key, MEM_SZ, 0666|IPC_CREAT);
+  if (shmid == -1) {
     perror("shmget falhou.");
-		exit(-1);
-	}
+    exit(-1);
+  }
 
-	void *shared_memory = shmat(shmid, (void*)0, 0);
-	if (shared_memory == (void *) -1) {
-		perror("shmat falhou.");
-		exit(-1);
-	}
+  void *shared_memory = shmat(shmid, (void*)0, 0);
+  if (shared_memory == (void *) -1) {
+    perror("shmat falhou.");
+    exit(-1);
+  }
 
-	printf("ID da memoria compartilhada   = %d\n", shmid);
-	printf("ADDR da memoria compartilhada = %p\n", shared_memory);
+  printf("ID da memoria compartilhada   = %d\n", shmid);
+  printf("ADDR da memoria compartilhada = %p\n", shared_memory);
 
-	sa_ptr = (sa_t *) shared_memory;
+  sa_ptr = (sa_t *) shared_memory;
 
-	sa_ptr->num=0;
-	for(i = 0; i < BUFF_SZ; i++)
-		sa_ptr->buffer[i] = 0;
+  sa_ptr->num=0;
+  for(i = 0; i < BUFF_SZ; i++)
+    sa_ptr->buffer[i] = 0;
 
-	for(;;) {
-		if (sa_ptr->num ==  0) {
-			for(i = 0; i < BUFF_SZ; i++)
-				sa_ptr->buffer[i] = '#';
-			printf("Produziu %zd bytes\n", i);
-			sa_ptr->num = i;
-		}
-	}
+  for(;;) {
+    if (sa_ptr->num ==  0) {
+      for(i = 0; i < BUFF_SZ; i++)
+        sa_ptr->buffer[i] = '#';
+      printf("Produziu %zd bytes\n", i);
+      sa_ptr->num = i;
+    }
+  }
   return 0;
 }
